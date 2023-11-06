ffce8e6fc11f9559813059ebd405b05dfcd8a39a
ibrahimajifajarromadhon <ajii4262@gmail.com>
2023-11-06 22:41:47 +0700

    Updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 // Create the store
 export default configureStore({
   reducer: rootReducers,
-  devTools: import.meta.env.NODE_ENV === "development",
+  // devTools: import.meta.env.NODE_ENV === "development",
   enhancers: [composeWithDevTools()],
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk), // It not necessary if your feature is not too complex, you can just comment this line if you don't need it
 });
