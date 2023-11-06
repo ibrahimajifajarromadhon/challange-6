bace147253da7b522ef1f407bf6d7ccee78faa5e
ibrahimajifajarromadhon <ajii4262@gmail.com>
2023-11-06 22:45:58 +0700

    Updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import rootReducers from "./reducers";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 // Create the store
 export default configureStore({
   reducer: rootReducers,
   // devTools: import.meta.env.NODE_ENV === "development",
-  enhancers: [composeWithDevTools()],
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk), // It not necessary if your feature is not too complex, you can just comment this line if you don't need it
 });
